5a88b2e4a745efe07d4948d3ad7da71a1508c80b
Bobby <zhang.deplives@hotmail.com>
2021-11-19 17:34:30 +0800

    reformat

--- zsh-command-not-found.plugin.zsh
+++ zsh-command-not-found.plugin.zsh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 
-if ! which brew > /dev/null; then return; fi
+if ! which brew >/dev/null; then return; fi
 
 homebrew_command_not_found_handle() {
 
     local cmd="$1"
 
-    if test -z "$CONTINUOUS_INTEGRATION" && test -n "$MC_SID" -o ! -t 1 ; then
-        [ -n "$BASH_VERSION" ] && \
+    if test -z "$CONTINUOUS_INTEGRATION" && test -n "$MC_SID" -o ! -t 1; then
+        [ -n "$BASH_VERSION" ] &&
             TEXTDOMAIN=command-not-found echo $"$cmd: command not found"
 
-        [ -n "$ZSH_VERSION" ] && [[ "$ZSH_VERSION" > "5.2" ]] && \
+        [ -n "$ZSH_VERSION" ] && [[ "$ZSH_VERSION" > "5.2" ]] &&
             echo "zsh: command not found: $cmd" >&2
         return 127
     fi
@@ -18,10 +18,10 @@ homebrew_command_not_found_handle() {
     local txt="$(brew which-formula --explain $cmd 2>/dev/null)"
 
     if [ -z "$txt" ]; then
-        [ -n "$BASH_VERSION" ] && \
+        [ -n "$BASH_VERSION" ] &&
             TEXTDOMAIN=command-not-found echo $"$cmd: command not found"
 
-        [ -n "$ZSH_VERSION" ] && [[ "$ZSH_VERSION" > "5.2" ]] && \
+        [ -n "$ZSH_VERSION" ] && [[ "$ZSH_VERSION" > "5.2" ]] &&
             echo "zsh: command not found: $cmd" >&2
     else
         echo "$txt"
